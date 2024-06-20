feabb0ded77872e88cf4c993d9b308f990e79473
Bruno Ricardo de Oliveira <bruno88.rick@hotmail.com>
2024-06-20 05:13:57 -0300

    Initial Commit

---
+++ SwiftDataDynamicallySortAndFilter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftDataDynamicallySortAndFilter
+//
+//  Created by Bruno Oliveira on 20/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftDataDynamicallySortAndFilter/SwiftDataDynamicallySortAndFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataDynamicallySortAndFilterApp.swift
+//  SwiftDataDynamicallySortAndFilter
+//
+//  Created by Bruno Oliveira on 20/06/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataDynamicallySortAndFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
